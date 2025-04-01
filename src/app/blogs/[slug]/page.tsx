c94f57e2050bb37f82dd97cc78ebdfa0899c68fc
shivam7703 <shivamg7703@gmail.com>
2025-04-01 17:43:03 +0530

    con

---
+++ src/app/blogs/[slug]/page.tsx
@@ -0,0 +1,19 @@
+import Banner from '@/components/global/banner'
+import Blogs from '@/components/home/blog';
+import React from 'react'
+
+function page() {
+  return (
+    <>
+      <Banner img ={""}
+                  title="Blogs Details"
+                  para="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit id adipisci laboriosam amet praesentium illum?"
+                  slug="slug here"/>
+                  <Blogs/>
+                  
+    </>
+   
+  )
+}
+
+export default page;
